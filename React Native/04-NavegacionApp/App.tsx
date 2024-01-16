8b1e41d0c539b1c122b0402a386f83257c63e6da
CarlosCo_Dev <condorpasa26@gmail.com>
2024-01-15 19:12:35 -0500

    iniciando navigation

---
+++ React Native/04-NavegacionApp/App.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+function App() {
+  return ( 
+    <View>
+      <Text>
+        Navegation App Screen
+      </Text>
+    </View>
+  );
+}
+
+export default App;
